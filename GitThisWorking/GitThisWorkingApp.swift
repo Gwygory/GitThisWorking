3c69e9ff9376229ad1008c046d94457a9f0a1f60
Mark Ramirez <mramirez@screamingbee.com>
2022-09-19 11:14:28 -0500

    Initial Commit

---
+++ GitThisWorking/GitThisWorkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitThisWorkingApp.swift
+//  GitThisWorking
+//
+//  Created by Mark Ramirez on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct GitThisWorkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
